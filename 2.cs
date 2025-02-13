b6ffe94c30e6e9d1b7b78bb5e36467c31e96f55f
Lebedeffson <111191095+Chui1281@users.noreply.github.com>
2025-02-13 16:09:17 +0300

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -1,24 +1,23 @@
 namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int n;
-            Console.Write("Введите число (не менее 2): ");
-            while (!int.TryParse(Console.ReadLine(), out n) || n < 2)
-            {
-                Console.WriteLine("Ошибка! Введите целое число не менее 2.");
-                Console.Write("Попробуйте снова: ");
-            }
 
-            for (int j = 2; j <= n + 1; j++)
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.Write("Введите число (не менее 2): ");
+        while (!int.TryParse(Console.ReadLine(), out n) || n < 2)
+        {
+            Console.WriteLine("Ошибка! Введите целое число не менее 2.");
+            Console.Write("Попробуйте снова: ");
+        }
+
+        for (int j = 2; j <= n + 1; j++)
+        {
+            if (n % j == 0)
             {
-                if (n % j == 0)
-                {
-                    Console.WriteLine(j);
-                    break;
-                }
+                Console.WriteLine(j);
+                break;
             }
         }
     }
