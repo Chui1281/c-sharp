c6778cfc5f2dbfc0b6da7f59f855d28c0e1c8392
Lebedeffson <111191095+Chui1281@users.noreply.github.com>
2025-02-13 15:22:08 +0300

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -6,14 +6,21 @@ class Program
     static void Main(string[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        for (int j = 2; j < n+1; j++)
+        if (n >= 2)
         {
-            if (n%j==0)
+            for (int j = 2; j < n + 1; j++)
             {
-                Console.WriteLine(j);
-                break;
+                if (n % j == 0)
+                {
+                    Console.WriteLine(j);
+                    break;
+                }
             }
-            
+        }
+        else
+        {
+            Console.WriteLine("Введенное число должно быть не меньше 2");
+                    
         }
     }
 }
