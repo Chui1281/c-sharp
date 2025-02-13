301d95bf73dedc418a373444c9dbdd9350f23a7f
Lebedeffson <111191095+Chui1281@users.noreply.github.com>
2025-02-13 15:10:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//exersize 1
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = Convert.ToInt32(Console.ReadLine());
+        List<int> array = new List<int>();
+        while(i!=0)
+        {
+            array.Add(i);
+            i=Convert.ToInt32(Console.ReadLine());
+        }
+        int result=0;
+        int  count = 1;
+        for (int j = 0; j < ((array.Count) - 1); j++)
+        {
+            if (array[j] == array[j + 1])
+            {
+                count++;
+            }
+            else
+            {
+                result=Math.Max(result, count);
+                count = 1;
+            }
+            
+        }
+        result=Math.Max(result, count);
+        Console.WriteLine(result);
+    }
+}
